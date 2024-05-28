4d2fe101d25d4a30cd05e7496572bcd9c8f95244
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-05-28 10:12:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ menu.addEventListener("click", () => {
 close.addEventListener("click", () => {
   nav.classList.remove("open-nav");
 });
+/*
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+};
+
+*/
